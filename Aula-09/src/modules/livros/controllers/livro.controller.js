71e406ba8460cf3b0266e1688c1d619360fbdee1
Lucas <lugabriell084@gmail.com>
2025-08-07 17:13:24 -0300

    Add: implementa rotas e validações para controle de livros, incluindo listagem, atualização e deleção

--- Aula-09/src/modules/livros/controllers/livro.controller.js
+++ Aula-09/src/modules/livros/controllers/livro.controller.js
@@ -16,7 +16,7 @@ class LivroController {
             if ( titulo.length < 2){
                 return res.status(400).json({msg: "Título deve ter pelo menos 2 caracteres"})
             }
-            if ( !["Romance", "Ficção", "Aventura", "Terror", "Fantasia", "Biografia", "Drama"].some(g => g === genero) ){
+            if ( !['Romance', 'Conto', 'Biografia', 'Fantasia', 'Ficção', 'Suspense', 'Terror', 'Aventura', 'Drama'].some(g => g === genero) ){
                 return res.status(400).json({ msg: "Gênero inválido" });
             }
             if ( typeof preco !== 'number' ){
@@ -28,6 +28,97 @@ class LivroController {
             res.status(500).json({ msg: "Erro no sistema. Tente novamente mais tarde!" });
         }
     }
+    static containsSQLInjection(str) {
+        const sqlInjectionPattern = /(['";])/;
+        return typeof str === 'string' && sqlInjectionPattern.test(str);
+    }
+    static async sql(req, res){
+        try {
+            const { titulo, autor, ano_publicacao, genero, preco } = req.body;
+
+            if ( this.containsSQLInjection(titulo)) {
+                return res.status(400).json({msg: 'Título inválido'});
+            }
+            if ( this.containsSQLInjection(autor)) {
+                return res.status(400).json({msg: 'Autor inválido'});
+            }
+            if ( this.containsSQLInjection(genero)) {
+                return res.status(400).json({msg: 'Gênero inválido'});
+            }
+            if ( typeof ano_publicacao !== 'number') {
+                return res.status(400).json({msg: 'Ano de publicação deve ser um número'});
+            }
+            if ( typeof preco !== 'number') {
+                return res.status(400).json({msg: 'Preço deve ser um número'});
+            }
+        } catch (error) {
+            res.status(500).json({msg: "Erro no sistema. Tente novamente mais tarde!"});
+        }
+    }
+    static async listarTodos(req, res){
+        try {
+            const livro = await LivroModel.findAll()
+            res.status(200).json(livro);
+        } catch (error) {
+            res.status(500).json({msg: "Erro no sistema. Tente novamente mais tarde!"});
+        }
+    }
+    static async listarPorNome(req, res){
+        try {
+            const {titulo} = req.query;
+            const livro = await LivroModel.findAll({where:{
+                titulo:{ [require('sequelize').Op.like]: `%${titulo}%`
+            }}});
+            if (livro.length === 0) {
+                return res.status(404).json({ msg: 'Livro não encontrado' });
+            }
+            res.status(200).json('Livro encontrado');
+        } catch (error) {
+            res.status(500).json({ msg: "Erro no sistema. Tente novamente mais tarde!", error: error.message}); 
+        }
+    }
+    static async listarPorId(req, res){
+        try {
+            const livro = await LivroModel.findByPk(req.params.id);
+            if(!livro){
+                return res.status(404).json({msg: 'Livro não encontrado'})
+            }
+            res.status(200).json({msg: 'Livro encontrado', livro})
+        } catch (error) {
+            res.status(500).json({ msg: "Erro no sistema. Tente novamente mais tarde!" });
+        }
+    }
+    static async atualizarLivro(req, res) {
+        try {
+        const { id } = req.params;
+        const { titulo, autor, ano_publicacao, genero, preco } = req.body;
+        if (  !titulo || !autor || !ano_publicacao || !genero || !preco ){
+            return res.status(400).json({msg: "Dados inválidos para atualização"})
+        }
+        const livro = await LivroModel.findByPk(id);
+        if (!livro) {
+            return res.status(404).json({ msg: 'Livro não encontrado' });
+        }
+        await livro.update({ titulo, autor, ano_publicacao, genero, preco });
+            res.status(200).json({msg: 'Livro atualizado com sucesso'})
+        } catch (error) {
+            res.status(500).json({ msg: "Erro no sistema. Tente novamente mais tarde!" });
+        }
+      }
+      static async deletarLivro(req, res) {
+        try {
+          const { id } = req.params;
+          const livro = await LivroModel.findByPk(id);
+          if (!livro) {
+            return res.status(404).json({ msg: 'Livro não encontrado' });
+          }
+          await livro.destroy();
+          res.status(200).json({ msg: 'Livro deletado com sucesso' });
+        } catch (error) {
+            res.status(500).json({ msg: "Erro no sistema. Tente novamente mais tarde!" });
+        }
+      }
+    
 
 }
 
--- Aula-09/src/modules/livros/routes/livro.route.js
+++ Aula-09/src/modules/livros/routes/livro.route.js
@@ -3,7 +3,17 @@ const livroController = require('../controllers/livro.controller');
 
 const router = express.Router();
 
-router.post('/', livroController.criarLivro)
+router.post('/', livroController.criarLivro);
+
+router.get('/', livroController.listarTodos);
+
+router.get('/busca', livroController.listarPorNome);
+
+router.get('/:id', livroController.listarPorId);
+
+router.put('/:id', livroController.atualizarLivro);
+
+router.delete('/:id', livroController.deletarLivro)
 
 
 module.exports = router
